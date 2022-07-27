7edba03408aec1d7afeed9e06023f5c7224791ef
Roe Greene <robert@byrojo.com>
2022-07-27 15:30:15 -0700

    add form data

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,86 +1,128 @@
-import { View, Text } from 'react-native'
-import React, { createContext, useContext, useState } from 'react'
-import * as WebBrowser from 'expo-web-browser';
-import { ResponseType } from 'expo-auth-session';
-import * as Google from 'expo-auth-session/providers/google';
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithCredential } from 'firebase/auth';
-import firebase from '../firebase'
-import { useNavigation } from '@react-navigation/native';
-
+import { View, Text } from "react-native";
+import React, { createContext, useContext, useState } from "react";
+import * as WebBrowser from "expo-web-browser";
+import { ResponseType } from "expo-auth-session";
+import * as Google from "expo-auth-session/providers/google";
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithCredential,
+} from "firebase/auth";
+import firebase from "../firebase";
+import { useNavigation } from "@react-navigation/native";
+import {
+  getFirestore,
+  addDoc,
+  setDoc,
+  collection,
+  getDocs,
+  where,
+} from "firebase/firestore";
+const db = getFirestore();
 // import * as Google from 'expo-google-app-auth';
 
 initializeApp({
-        apiKey: "AIzaSyBOaUNediehSWV0DUpKxhWfqM97fR9GXFM",
-        authDomain: "access-adventure.firebaseapp.com",
-        projectId: "access-adventure",
-        storageBucket: "access-adventure.appspot.com",
-        messagingSenderId: "12457190458",
-        appId: "1:12457190458:web:699a916d9d0300ccc98103"
-       });
+  apiKey: "AIzaSyBOaUNediehSWV0DUpKxhWfqM97fR9GXFM",
+  authDomain: "access-adventure.firebaseapp.com",
+  projectId: "access-adventure",
+  storageBucket: "access-adventure.appspot.com",
+  messagingSenderId: "12457190458",
+  appId: "1:12457190458:web:699a916d9d0300ccc98103",
+});
 
 WebBrowser.maybeCompleteAuthSession();
 
-const AuthContext = createContext({})
-
+const AuthContext = createContext({});
+let isWriting = false;
 export const AuthProvider = ({ children }) => {
-  const {navigate} = useNavigation();
-  const [user, setUser] = useState()
+  const { navigate } = useNavigation();
+  const [user, setUser] = useState();
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    clientId:
+      "12457190458-ad50bq5o9k7grumjsb8an8806e8nj9th.apps.googleusercontent.com",
+  });
 
-    const [request, response, promptAsync] = Google.useIdTokenAuthRequest(
-        {
-          clientId: '12457190458-ad50bq5o9k7grumjsb8an8806e8nj9th.apps.googleusercontent.com',
-          },
-      );
+  React.useEffect(() => {
+    if (response?.type === "success" && !isWriting) {
+      isWriting = true;
+      const { id_token } = response.params;
+      const auth = getAuth();
+      const credential = GoogleAuthProvider.credential(id_token);
+      signInWithCredential(auth, credential).then((value) => {
+        const writeUser = () => {
+          addDoc(collection(db, "users"), {
+            email: value.user.email,
+            firstName: value.user.displayName.split(" ")[0],
+            lastName: value.user.displayName.split(" ")[1],
+            photoUrl: value.user.photoURL,
+            uid: value.user.uid,
+          }).finally(() => {
+            isWriting = false;
+          });
+          setUser(value.user);
+          navigate("Home");
+        };
 
-    React.useEffect(() => {
-        console.log(response);
-        if (response?.type === 'success') {
-          const { id_token } = response.params;
-          const auth = getAuth();
-          const credential = GoogleAuthProvider.credential(id_token);
-          signInWithCredential(auth, credential).then((value) => {
-            setUser(value.user)
-            navigate('Home')
+        //check to see if a user exists
+        getDocs(
+          collection(db, "users"),
+          where("uid", "==", value.user.uid)
+        )
+          .then((data) => {
+            let userDoc = []
+            data.forEach(doc => {
+              if( doc.udid === value.user.udid) {
+                userDoc = doc
+              }
+            })
+            if(!userDoc)
+              writeUser();
+            else {
+              setUser(value.user);
+              navigate("Home");
+            }
           })
-        }
-      }, [response]);
+          .catch((e) => {
+            console.error(e)
+            writeUser();
+          });
+      });
+    }
+  }, [response]);
 
+  // const signInWithGoogle = async () => {
+  //     Google.useAuthRequest(config).then( async (logInResult) => {
+  //         if(logInResult.type === 'success') {
 
-    // const signInWithGoogle = async () => {
-    //     Google.useAuthRequest(config).then( async (logInResult) => {
-    //         if(logInResult.type === 'success') {
+  //         }
+  //     })
+  // }
 
-    //         }
-    //     })
-    // }
+  // const [request, response, promptAsync] = Google.useAuthRequest({
+  //     iosClientId: '12457190458-hber30k6am5n1m7f6kuavcdogusbdifd.apps.googleusercontent.com',
+  //     expoClientId: '12457190458-hber30k6am5n1m7f6kuavcdogusbdifd.apps.googleusercontent.com',
+  //   });
 
-    // const [request, response, promptAsync] = Google.useAuthRequest({
-    //     iosClientId: '12457190458-hber30k6am5n1m7f6kuavcdogusbdifd.apps.googleusercontent.com',
-    //     expoClientId: '12457190458-hber30k6am5n1m7f6kuavcdogusbdifd.apps.googleusercontent.com',
-    //   });
-
-    // const signInWithGoogle = async () => {
-    //     if (response?.type === 'success') {
-    //       const { authentication } = response;
-    //       console.log(response);
-    //       }
-    //   }
-
-
+  // const signInWithGoogle = async () => {
+  //     if (response?.type === 'success') {
+  //       const { authentication } = response;
+  //       console.log(response);
+  //       }
+  //   }
 
   return (
-    <AuthContext.Provider 
-        value={{
-            user,
-            signInWithGoogle:() => promptAsync()
-        }}
+    <AuthContext.Provider
+      value={{
+        user,
+        signInWithGoogle: () => promptAsync(),
+      }}
     >
-        { children }
+      {children}
     </AuthContext.Provider>
-  )
+  );
 };
 
 export default function useAuth() {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 }
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,47 +1,107 @@
-import React from 'react'
-import { View } from "react-native"
-import { Avatar, Button, TextInput } from 'react-native-paper';
+import React, { useEffect, useState, useRef } from "react";
+import { View } from "react-native";
+import { Avatar, Button, TextInput } from "react-native-paper";
+import {
+  getFirestore,
+  addDoc,
+  setDoc,
+  getDoc,
+  doc,
+  collection,
+  where,
+  getDocs,
+  updateDoc
+} from "firebase/firestore";
+import useAuth from "../hooks/useAuth";
+const db = getFirestore();
 
-const ProfileScreen = ({navigation}) => { 
-    const [formData, setFormData] = React.useState({
-        email: '',
-        firstName: '',
-        lastName: '',
-    });
-    return (
-        <View style={{alignItems: "center", paddingTop: 16}}>
-            <Avatar.Image size={100} source={require('../assets/adaptive-icon.png')} />
-            <View style={{ padding: 16, flexDirection: "row"}}>
-                <TextInput
-                label="First Name"
-                value={formData.firstName}
-                style={{ flex: 1}}
-                onChangeText={text => setFormData({...formData, email: text})}
-                />
-                
-            </View>
-            <View style={{ padding: 16, flexDirection: "row"}}>
-                <TextInput
-                label="Last Name"
-                value={formData.lastName}
-                style={{ flex: 1}}
-                onChangeText={text => setFormData({...formData, email: text})}
-                />
-                
-            </View>
-            <View style={{ padding: 16, flexDirection: "row"}}>
-                <TextInput
-                label="Email"
-                value={formData.email}
-                style={{ flex: 1}}
-                onChangeText={text => setFormData({...formData, email: text})}
-                />
-                
-            </View>
-            <View style={{ padding: 16, flexDirection: "row"}}>
-                <Button icon="floppy" mode="contained" onPress={() => {}}>Save</Button>
-          </View>
-        </View>
-    )
-}
-export default ProfileScreen
+const ProfileScreen = ({ navigation }) => {
+  const { user } = useAuth();
+  const [formData, setFormData] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+  });
+
+  const userDocRef = useRef();
+
+  useEffect(() => {
+    if (user.uid && !userDocRef.current) {
+      getDocs(collection(db, "users"), where("uid", "==", user.uid))
+        .then((data) => {
+         
+          userDocRef.current = data.docs[0];
+          const userData = data.docs[0].data()
+    
+          setFormData({
+            email: userData.email,
+            firstName: userData.firstName,
+            lastName: userData.lastName,
+          })
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    }
+  }, [user.uid]);
+
+  const [isFetching, setIsFetching] = useState(false);
+
+  const onSubmit = () => {
+    setIsFetching(true);
+    if (userDocRef.current) {
+      if (formData.email) userDocRef.current.email = formData.email;
+      if (formData.firstName) userDocRef.current.firstName = formData.firstName;
+      if (formData.lastName) userDocRef.current.lastName = formData.lastName;
+   
+      updateDoc(doc(db, "users", userDocRef.current.id), formData)
+        .then(() => {})
+        .catch((e) => console.error(e))
+        .finally(() => setIsFetching(false));
+    }
+  };
+
+  return (
+    <View style={{ alignItems: "center", paddingTop: 16 }}>
+      <Avatar.Image
+        size={100}
+        source={require("../assets/adaptive-icon.png")}
+      />
+      <View style={{ padding: 16, flexDirection: "row" }}>
+        <TextInput
+          label="First Name"
+          value={formData.firstName}
+          style={{ flex: 1 }}
+          onChangeText={(text) => setFormData({ ...formData, firstName: text })}
+        />
+      </View>
+      <View style={{ padding: 16, flexDirection: "row" }}>
+        <TextInput
+          label="Last Name"
+          value={formData.lastName}
+          style={{ flex: 1 }}
+          onChangeText={(text) => setFormData({ ...formData, lastName: text })}
+        />
+      </View>
+      <View style={{ padding: 16, flexDirection: "row" }}>
+        <TextInput
+          label="Email"
+          value={formData.email}
+          style={{ flex: 1 }}
+          onChangeText={(text) => setFormData({ ...formData, email: text })}
+        />
+      </View>
+      <View style={{ padding: 16, flexDirection: "row" }}>
+        <Button
+          disabled={isFetching}
+          icon="floppy"
+          mode="contained"
+          onPress={onSubmit}
+        >
+          Save
+        </Button>
+      </View>
+    </View>
+  );
+};
+export default ProfileScreen;
